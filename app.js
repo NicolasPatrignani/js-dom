5023b0d72d5eafb87b5fcaca566f2919a0a13f12
Nicolas Patrignani <nicolaspatrignani@hotmail.com>
2022-10-19 16:36:23 -0300

    working with DOM

--- app.js
+++ app.js
@@ -6,18 +6,33 @@ alert();
 window.alert(); 
 */
 
-document.getElementById('some-id') //seleccione un elemento HTML por su ID (selecciona solo un elemento, ya que las ID deben ser únicas)
+//document.getElementById('some-id') //seleccione un elemento HTML por su ID (selecciona solo un elemento, ya que las ID deben ser únicas)
 
-document.querySelector('<some-css-selector>') //Selecciona el primer elemento HTML coincidente (!) que cumple/selecciona el selector de CSS provisto; El selector de CSS puede ser básicamente cualquier tipo de selector de CSS válido (por ejemplo, selector de ID, selector de tipo de etiqueta, selector de clase, selectores combinados, etc.)
+//document.querySelector('<some-css-selector>') //Selecciona el primer elemento HTML coincidente (!) que cumple/selecciona el selector de CSS provisto; El selector de CSS puede ser básicamente cualquier tipo de selector de CSS válido (por ejemplo, selector de ID, selector de tipo de etiqueta, selector de clase, selectores combinados, etc.)
 
-document.querySelectorAll('<some-css-selector>') //selecciona TODOS los elementos HTML coincidentes que se cumplen/seleccionan mediante el selector de CSS proporcionado
+//document.querySelectorAll('<some-css-selector>') //selecciona TODOS los elementos HTML coincidentes que se cumplen/seleccionan mediante el selector de CSS proporcionado
 
-document.getElementsByClassName('some-css-class') //selecciona todos los elementos HTML que tienen la clase CSS proporcionada
+//document.getElementsByClassName('some-css-class') //selecciona todos los elementos HTML que tienen la clase CSS proporcionada
 
-document.getElementsByTagName('tag') //selecciona todos los elementos HTML que son del tipo de etiqueta HTML proporcionado
+//document.getElementsByTagName('tag') //selecciona todos los elementos HTML que son del tipo de etiqueta HTML proporcionado
 
 let anchorElement = document.getElementById('external-link');
 anchorElement.href = 'https://google.com';
 
 anchorElement = document.querySelector('p a');
 anchorElement.href = 'https://academind.com';
+
+// ADD AN ELEMENT
+// 1. Create new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://google.com';
+newAnchorElement.textContent = 'This leads to Google!'
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
